4ed75708205fcb6fdec6648aabe6140979626e91
Lokesh <lokesh.loki.codes@gmail.com>
2025-07-26 02:15:50 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -41,7 +41,7 @@ export const nodeCache = new NodeCache();
 
 const app = express();
 
-app.use(cors({
+app.use(cors({ "*"
     // origin: [ "https://lokicart-mern.netlify.app/", "http://localhost:5173" ], 
     // methods: ["GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"],
     // allowedHeaders: ["Content-Type", "Origin", "Authorization"],
