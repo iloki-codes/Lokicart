39374feb3f1b4cd48d3682286208536706631da7
Lokesh <lokesh.loki.codes@gmail.com>
2025-07-25 15:33:12 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -66,9 +66,9 @@ app.use(cors({
 //     methods: [ "GET", "POST", "PUT", "DELETE", "OPTIONS"],
 //     allowedHeaders: [ "Content-Type", "Authorization" ]
 // }));
-app.options(origin, (req: Request, res: Response) => {
-    res.header("Access-Control-Allow-Origin", req.headers.origin);
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE,HEAD, OPTIONS");
+app.options("*", (req: Request, res: Response) => {
+    res.header("Access-Control-Allow-Origin", "https://lokicart-mern.netlify.app/");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, HEAD, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     res.header("Access-Control-Allow-Credentials", "true");
     res.sendStatus(200);
